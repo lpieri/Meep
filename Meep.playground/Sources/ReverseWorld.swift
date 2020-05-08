e9836be1e5ea227c319bfbd1d09f5d27dd49d89e
Louise P <cpieri@student.42.fr>
2020-05-08 16:16:46 +0200

    [Add]: Import assets files and create ReverseWorld.swift

---
+++ Meep.playground/Sources/ReverseWorld.swift
@@ -0,0 +1,9 @@
+import SpriteKit
+
+public class ReverseWorld: SKScene {
+    
+    public override func didMove(to view: SKView) {}
+    
+    public override func update(_ currentTime: TimeInterval) {}
+    
+}
