a2c70e9a36399ae5934482c5da9ca980695feae0
Louise P <cpieri@student.42.fr>
2020-05-08 15:19:18 +0200

    [Init]: Create 2 differents scene and try to add files in sources

---
+++ Meep.playground/Sources/NormalWorld.swift
@@ -0,0 +1,9 @@
+import SpriteKit
+
+public class NormalWorld: SKScene {
+    
+    public override func didMove(to view: SKView) {}
+    
+    public override func update(_ currentTime: TimeInterval) {}
+    
+}
