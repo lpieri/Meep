b172ee07c4f3abc2a3de6a5d1ecf5867bae3239a
Louise P <cpieri@student.42.fr>
2020-05-08 19:00:05 +0200

    [Update]: Starting the prog of the game

--- Meep.playground/Contents.swift
+++ Meep.playground/Contents.swift
@@ -3,16 +3,67 @@
 import PlaygroundSupport
 import SpriteKit
 
+public class GameWorld: SKScene {
+    
+    enum macOSKeyMap: UInt16 {
+        case leftArrow = 123
+        case rightArrow = 124
+        case upArrow = 126
+        case downArrow = 125
+    }
+    
+    public let macOSKeyToFunc: [UInt16:(String) -> Void] = []
+    
+    public var levelReference: SKReferenceNode!
+    public var levelName: String!
+    public var player: SKSpriteNode!
+    
+    public override func didMove(to view: SKView) {
+        // Get label node from scene and store it for use later
+        
+        levelReference = childNode(withName: "//LevelReference") as? SKReferenceNode
+        levelName = "Reverse"
+        levelReference.resolve()
+        player = childNode(withName: "//Meep") as? SKSpriteNode
+        print(player)
+    }
+    
+    @objc static public override var supportsSecureCoding: Bool {
+        // SKNode conforms to NSSecureCoding, so any subclass going
+        // through the decoding process must support secure coding
+        get {
+            return true
+        }
+    }
+    
+    func runPlayer(level: String) {
+        let xAddValue: CGFloat = level == "Reverse" ? -30 : 30
+        player.position.x += xAddValue
+    }
+    
+    #if os(macOS)
+    public override func keyDown(with event: NSEvent) {
+        let key = event.keyCode
+        switch key {
+        case macOSKeyMap.leftArrow.rawValue:
+            runPlayer(level: "Reverse")
+        default:
+            print(key)
+        }
+    }
+    #endif
+    
+    public override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
+
 let Width = 1024
 let Height = 768
 
-// Load the SKScene from 'GameScene.sks'
 let sceneView = SKView(frame: CGRect(x:0 , y:0, width: Width, height: Height))
 if let scene = GameWorld(fileNamed: "GameScene") {
-    // Set the scale mode to scale to fit the window
     scene.scaleMode = .aspectFill
-    
-    // Present the scene
     sceneView.presentScene(scene)
 }
 
--- Meep.playground/Sources/GameWorld.swift
+++ Meep.playground/Sources/GameWorld.swift
@@ -1,56 +1,25 @@
 import SpriteKit
 
-public class GameWorld: SKScene {
-    
-    public override func didMove(to view: SKView) {
-        // Get label node from scene and store it for use later
-    }
-    
-    @objc static public override var supportsSecureCoding: Bool {
-        // SKNode conforms to NSSecureCoding, so any subclass going
-        // through the decoding process must support secure coding
-        get {
-            return true
-        }
-    }
-    
-    func touchDown(atPoint pos : CGPoint) {
-//        guard let n = spinnyNode.copy() as? SKShapeNode else { return }
-//
-//        n.position = pos
-//        n.strokeColor = SKColor.green
-//        addChild(n)
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-//        guard let n = self.spinnyNode.copy() as? SKShapeNode else { return }
-//
-//        n.position = pos
-//        n.strokeColor = SKColor.blue
-//        addChild(n)
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-//        guard let n = spinnyNode.copy() as? SKShapeNode else { return }
-//
-//        n.position = pos
-//        n.strokeColor = SKColor.red
-//        addChild(n)
-    }
-    
-    public override func mouseDown(with event: NSEvent) {
-        touchDown(atPoint: event.location(in: self))
-    }
-    
-    public override func mouseDragged(with event: NSEvent) {
-        touchMoved(toPoint: event.location(in: self))
-    }
-    
-    public override func mouseUp(with event: NSEvent) {
-        touchUp(atPoint: event.location(in: self))
-    }
-    
-    public override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
-}
+//public class GameWorld: SKScene {
+//    
+//    public var levelReference: SKReferenceNode!
+//    
+//    public override func didMove(to view: SKView) {
+//        // Get label node from scene and store it for use later
+//        
+//        levelReference = childNode(withName: "//LevelReference") as? SKReferenceNode
+//        levelReference.resolve()
+//    }
+//    
+//    @objc static public override var supportsSecureCoding: Bool {
+//        // SKNode conforms to NSSecureCoding, so any subclass going
+//        // through the decoding process must support secure coding
+//        get {
+//            return true
+//        }
+//    }
+//    
+//    public override func update(_ currentTime: TimeInterval) {
+//        // Called before each frame is rendered
+//    }
+//}
