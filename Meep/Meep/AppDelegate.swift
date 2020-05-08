bcd541ab0c78b768522c6fa37b4d10e6d30da0f6
Louise P <cpieri@student.42.fr>
2020-05-08 13:37:13 +0200

    [Init]: Create Xcode Project

---
+++ Meep/Meep/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Meep
+//
+//  Created by Louise on 08/05/2020.
+//  Copyright © 2020 Louise Pieri. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
