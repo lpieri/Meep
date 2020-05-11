617074847e2562edfe46b0780e5fd6614a948ee5
Louise P <cpieri@student.42.fr>
2020-05-11 14:59:20 +0200

    lol

--- Meep.playground/Contents.swift
+++ Meep.playground/Contents.swift
@@ -12,6 +12,18 @@ Meep is a transgender monster...
 She dreams of becoming a pink monster in the other universe.
 """
 
+public class FlyingPlatform: SKSpriteNode {
+    
+    public func flying() {
+        let upValue: CGFloat = 240
+        let flyDown = SKAction.moveTo(y: self.position.y - upValue, duration: 1)
+        let flyUp = SKAction.moveTo(y: self.position.y + upValue, duration: 1)
+        let flyingActionSequence = SKAction.sequence([flyDown, .wait(forDuration: 3), flyUp, .wait(forDuration: 3)])
+        self.run(.repeatForever(flyingActionSequence))
+    }
+    
+}
+
 public class GameWorld: SKScene {
     
     enum macOSKeyMap: UInt16 {
@@ -157,6 +169,12 @@ public class GameWorld: SKScene {
     #endif
     
     public override func update(_ currentTime: TimeInterval) {
+        enumerateChildNodes(withName: "FlyingPlatform") {
+            node, stop in
+            if let piece = node as? FlyingPlatform { // Cast to Sprite if we can
+                piece.flying()
+            }
+        }
         if player.position.x < cameraNode.position.x {
             if player.position.x > -1536 {
                 cameraNode.position = .init(x: player.position.x, y: 0)
