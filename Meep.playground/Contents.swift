f742e78d54577ba291386723c0ae8ac460264649
Louise P <cpieri@student.42.fr>
2020-05-11 16:12:57 +0200

    wip

--- Meep.playground/Contents.swift
+++ Meep.playground/Contents.swift
@@ -62,9 +62,24 @@ public class GameWorld: SKScene {
         }
     }
     
+    func flying(platform: SKSpriteNode) {
+        let upValue: CGFloat = 150
+        let flyDown = SKAction.moveTo(y: self.position.y - upValue, duration: 1)
+        let flyUp = SKAction.moveTo(y: self.position.y + upValue, duration: 1)
+        let flyingActionSequence = SKAction.sequence([flyDown, .wait(forDuration: 3), flyUp, .wait(forDuration: 3)])
+        platform.run(.repeatForever(flyingActionSequence))
+    }
+    
     public override func didMove(to view: SKView) {
         
         let rotatePlatformActionSequence = SKAction.sequence([rotatePlatformAction(), .wait(forDuration: 2), rotatePlatformAction(), .wait(forDuration: 2)])
+        
+        enumerateChildNodes(withName: "//FlyingPlatform") {
+            node, stop in
+            if let platform = node as? SKSpriteNode {
+                self.flying(platform: platform)
+            }
+        }
         
         self.run(writingHistory())
         player = childNode(withName: "//Meep") as? SKSpriteNode
@@ -76,7 +91,6 @@ public class GameWorld: SKScene {
         cameraNode.position = .init(x: x, y: frame.midY)
         camera = cameraNode
         addChild(cameraNode)
-        
     }
     
     @objc static public override var supportsSecureCoding: Bool {
@@ -145,12 +159,7 @@ public class GameWorld: SKScene {
     #endif
     
     public override func update(_ currentTime: TimeInterval) {
-        enumerateChildNodes(withName: "//FlyingPlatform") {
-            node, stop in
-            if let piece = node as? FlyingPlatform { // Cast to Sprite if we can
-                piece.flying()
-            }
-        }
+        
         if player.position.x < cameraNode.position.x {
             if player.position.x > -1536 {
                 cameraNode.position = .init(x: player.position.x, y: 0)
