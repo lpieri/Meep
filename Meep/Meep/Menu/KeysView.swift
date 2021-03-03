812efee8433622a39ace3487167bce47460d1282
Louise P <cpieri@student.42.fr>
2021-03-03 15:37:17 +0100

    [Update]: Change the keyboard menu

--- Meep/Meep/Menu/KeysView.swift
+++ Meep/Meep/Menu/KeysView.swift
@@ -11,6 +11,15 @@ import SwiftUI
 struct KeysView: View {
     
     @State var menuView: Bool = false
+    @State var changeTouch: Bool = false
+    
+    func changeKey(touch: String) {
+        self.changeTouch.toggle()
+        if changeTouch {
+            print("touch:", touch, changeTouch)
+            self.changeTouch.toggle()
+        }
+    }
     
     var body: some View {
         ZStack {
@@ -26,10 +35,53 @@ struct KeysView: View {
                     .foregroundColor(.red)
                     .position(x: 30, y: 30)
                     
-                    VStack {
-                        Image("keys_logo")
+                    VStack(alignment: .center, spacing: 20) {
                         Spacer()
-                        Image("keyboard_touch")
+                        Text("Keyboard Menu :").font(.largeTitle)
+                        VStack(alignment: .center, spacing: 5) {
+                            HStack(alignment: .center) {
+                                Text("Jump:").font(.title)
+                                Spacer()
+                                Button("Up Arrow", action: {
+                                    changeKey(touch: "jump")
+                                })
+                            }
+                            HStack(alignment: .center) {
+                                Text("Crouch:").font(.title)
+                                Spacer()
+                                Button("Down Arrow", action: {
+                                    changeKey(touch: "crouch")
+                                })
+                            }
+                            HStack(alignment: .center) {
+                                Text("Run Right:").font(.title)
+                                Spacer()
+                                Button("Right Arrow", action: {
+                                    changeKey(touch: "run-right")
+                                })
+                            }
+                            HStack(alignment: .center) {
+                                Text("Run Left:").font(.title)
+                                Spacer()
+                                Button("Left Arrow", action: {
+                                    changeKey(touch: "run-left")
+                                })
+                            }
+                            HStack(alignment: .center) {
+                                Text("Diagonal jump left:").font(.title)
+                                Spacer()
+                                Button("Z Touch", action: {
+                                    changeKey(touch: "diag-jump-left")
+                                })
+                            }
+                            HStack(alignment: .center) {
+                                Text("Diagonal jump right:").font(.title)
+                                Spacer()
+                                Button("X Touch", action: {
+                                    changeKey(touch: "diag-jump-right")
+                                })
+                            }
+                        }.frame(width: 400, height: .none, alignment: .center)
                         Spacer()
                     }.padding()
                 }
